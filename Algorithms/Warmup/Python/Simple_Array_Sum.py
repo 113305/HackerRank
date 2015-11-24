319dca6b67bc66331ddc22863b2c9f3ac952314f
113305 <113305@naver.com>
2015-11-24 13:06:28 +0900

    Simple_Array_Sum

---
+++ Algorithms/Warmup/Python/Simple_Array_Sum.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+result = 0
+for a in arr:
+    result += a
+print(result)
